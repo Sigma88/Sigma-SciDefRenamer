9c189f729450e9c9fd65985eba063dd7f59475e5
Sigma88 <sigmamodexpansions@outlook.com>
2017-05-04 11:32:38 +0200

    untested changes

--- [Source]/SciDefRenamer/SciDefRenamer.cs
+++ [Source]/SciDefRenamer/SciDefRenamer.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System;
 using System.Reflection;
 using System.Linq;
@@ -15,25 +15,74 @@ namespace SigmaSciDefRenamer
         {
             foreach (ConfigNode SSDR in GameDatabase.Instance.GetConfigNodes("SigmaSciDefRenamer"))
             {
-                foreach (ConfigNode rename in SSDR.GetNodes("Rename"))
+                foreach (ConfigNode node in SSDR.nodes)
                 {
-                    string OLD = rename.GetValue("OLD");
-                    string NEW = rename.GetValue("NEW");
-                    foreach (ConfigNode config in GameDatabase.Instance.GetConfigNodes("EXPERIMENT_DEFINITION"))
+                    if (node.name == "Copy")
                     {
-                        ConfigNode results = config.GetNode("RESULTS");
-                        ConfigNode data = new ConfigNode();
-                        foreach (ConfigNode.Value key in results.values)
-                        {
-                            if (key.name.StartsWith(OLD))
-                            {
-                                data.AddValue(key.name.Replace(OLD, NEW), key.value);
-                            }
-                        }
-                        results.RemoveValuesStartWith(OLD);
-                        results.AddData(data);
+                        string OLD = rename.GetValue("OLD");
+                        string NEW = rename.GetValue("NEW");
+                        Copy(OLD, NEW);
+                    }
+                    if (node.name == "Delete")
+                    {
+                        string NAME = rename.GetValue("NAME");
+                        Delete(OLD);
+                    }
+                    if (node.name == "Rename")
+                    {
+                        string OLD = rename.GetValue("OLD");
+                        string NEW = rename.GetValue("NEW");
+                        Copy(OLD, NEW);
+                        Delete(OLD);
+                    }
+                    if (node.name == "Replace")
+                    {
+                        string OLD = rename.GetValue("OLD");
+                        string NEW = rename.GetValue("NEW");
+                        Replace(OLD, NEW);
                     }
                 }
+            }
+        }
+        void Copy(string OLD, string NEW)
+        {
+            foreach (ConfigNode config in GameDatabase.Instance.GetConfigNodes("EXPERIMENT_DEFINITION"))
+            {
+                ConfigNode results = config.GetNode("RESULTS");
+                ConfigNode data = new ConfigNode();
+                foreach (ConfigNode.Value key in results.values)
+                {
+                    if (key.name.StartsWith(OLD))
+                    {
+                        data.AddValue( NEW + key.name.Replace(0, OLD.Length), key.value);
+                    }
+                }
+                results.AddData(data);
+            }
+        }
+        void Delete(string NAME)
+        {
+            foreach (ConfigNode config in GameDatabase.Instance.GetConfigNodes("EXPERIMENT_DEFINITION"))
+            {
+                ConfigNode results = config.GetNode("RESULTS");
+                results.RemoveValuesStartWith(NAME);
+            }
+        }
+        void Replace(string OLD, string NEW)
+        {
+            foreach (ConfigNode config in GameDatabase.Instance.GetConfigNodes("EXPERIMENT_DEFINITION"))
+            {
+                ConfigNode results = config.GetNode("RESULTS");
+                ConfigNode data = new ConfigNode();
+                foreach (ConfigNode.Value key in results.values)
+                {
+                    if (key.name.StartsWith(OLD))
+                    {
+                        data.AddValue(key.name.Replace(OLD, NEW), key.value);
+                    }
+                }
+                results.RemoveValuesStartWith(OLD);
+                results.AddData(data);
             }
         }
     }
