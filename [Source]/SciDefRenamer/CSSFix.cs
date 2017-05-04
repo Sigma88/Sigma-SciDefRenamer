65689e211d6f9c6c60c5c9ae382887e1f606c76d
Sigma88 <sigmamodexpansions@outlook.com>
2017-05-04 02:08:51 +0200

    CSSFix

---
+++ [Source]/SciDefRenamer/CSSFix.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace CSSFix
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class CSSFix: MonoBehaviour
+    {
+        void Start()
+        {
+            foreach (ConfigNode config in GameDatabase.Instance.GetConfigNodes("EXPERIMENT_DEFINITION"))
+            {
+                ConfigNode results = config.GetNode("RESULTS");
+                ConfigNode data = new ConfigNode();
+                foreach (ConfigNode.Value key in results.values)
+                {
+                    data.AddValue(key.name + (key.name != "default" ? "*" : ""), key.value);
+                }
+                results.ClearData();
+                results.AddData(data);
+            }
+        }
+    }
+}
--- [Source]/SciDefRenamer/Properties/AssemblyInfo.cs
+++ [Source]/SciDefRenamer/Properties/AssemblyInfo.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("SciDefRenamer")]
-[assembly: AssemblyDescription("Changes the target planet for selected Science Definitions")]
+[assembly: AssemblyTitle("CSSFix")]
+[assembly: AssemblyDescription("CSS Fix")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: KSPAssembly("SciDefRenamer", 1, 0)]
+[assembly: KSPAssembly("CSSFix", 1, 0)]
 [assembly: AssemblyVersion("1.0.0")]
 
