96fb4bbc6dd84cab33344a9ff5d14df2a9c15982
Sigma88 <sigmamodexpansions@outlook.com>
2017-10-18 11:43:04 +0200

    update to v0.3.2

---
+++ [Source]/SciDefRenamer/Version.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+namespace SigmaSciDefRenamer
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Version : MonoBehaviour
+    {
+        public static readonly System.Version number = new System.Version("0.3.2");
+
+        void Awake()
+        {
+            Debug.Log("[SigmaLog] Version Check:   Sigma SciDefRenamer v" + number);
+        }
+    }
+}
